270a7b62f61707e73d723bb6e6168a6e93372d4b
Cristhian Valladolid <cmvc99@gmail.com>
2023-08-19 00:35:49 -0500

    Controlador de producto previo a insertar, repositoriry y service creados

---
+++ src/main/java/com/proyecto/ecommerce/springecommerce/controllers/producto.java
@@ -0,0 +1,42 @@
+package com.proyecto.ecommerce.springecommerce.controllers;
+import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.proyecto.ecommerce.springecommerce.models.Producto;
+import com.proyecto.ecommerce.springecommerce.models.Usuario;
+import com.proyecto.ecommerce.springecommerce.service.ProductoService;
+
+
+@Controller
+@RequestMapping("/productos")
+public class producto {
+
+    private final Logger logger = LoggerFactory.getLogger(producto.class);
+    
+    @Autowired
+    private ProductoService productoService;
+    
+    
+    @GetMapping("")
+    public String show(){
+        return "productos/show";
+    }
+
+    @GetMapping("/create")
+    public String create(){
+        return "productos/create";
+    }
+
+    @PostMapping("/save")
+    public String save(Producto producto){
+        logger.info("Objeto producto: {}", producto);
+        Usuario u = new Usuario(1, "", "", "", "", "", "", "", "", "");
+        
+        productoService.save(producto);
+        return "redirect:/productos";
+    }
+}
--- src/main/java/com/proyecto/ecommerce/springecommerce/models/Usuario.java
+++ src/main/java/com/proyecto/ecommerce/springecommerce/models/Usuario.java
@@ -1,15 +1,20 @@
 package com.proyecto.ecommerce.springecommerce.models;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
 import java.util.List; 
 
 
 @Data
+@AllArgsConstructor // Genera un constructor con todos los campos
 @Entity
 @Table(name= "usuarios") //Anotacion para no usar el nombre de la clase "Usuario"
 
 public class Usuario {
+    public Usuario(int idUsuario2, String nombre2, String username2, String email2, String direccion2, String telefono2,
+            String tipo2, String password2, String string, String string2) {
+    }
+
     //Atributos
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
