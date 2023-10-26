0c7d77a2e53bd95edbf965596392bc79b4c134bb
Cristhian Valladolid <cmvc99@gmail.com>
2023-10-25 23:32:01 -0500

    Detalle del producto 50% desde vista cliente pagina principal, envia el id y se recibe correctamente

--- src/main/java/com/proyecto/ecommerce/springecommerce/controllers/homeController.java
+++ src/main/java/com/proyecto/ecommerce/springecommerce/controllers/homeController.java
@@ -1,9 +1,13 @@
 package com.proyecto.ecommerce.springecommerce.controllers;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.proyecto.ecommerce.springecommerce.service.ProductoService;
@@ -12,6 +16,8 @@ import com.proyecto.ecommerce.springecommerce.service.ProductoService;
 @RequestMapping("/")
 public class homeController {
     
+    private final Logger log= LoggerFactory.getLogger(homeController.class);
+    
     @Autowired
     private ProductoService productoService;
 
@@ -19,6 +25,11 @@ public class homeController {
     public String home(Model model ){
         model.addAttribute("productos", productoService.findAll()); //guardar todos los datos de productos en variable productos
         return "usuario/home";
+    }
 
+    @GetMapping("productohome/{id}")
+    public String productoHome(@PathVariable Integer id){
+        log.info("IdProducto enviado como parametro (vista cliente pag principal a detalle): ", id );
+        return "usuario/productohome";
     }
 }
