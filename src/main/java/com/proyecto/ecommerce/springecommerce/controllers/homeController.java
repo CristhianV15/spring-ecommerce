9f8cfb6e6651019fa8c5ca4a6f97401fd01510ed
Cristhian Valladolid <cmvc99@gmail.com>
2023-11-12 20:54:02 -0500

    Se soluciono bug de pagina principal, falta solucionar variable usuario en cada controlador para el re-direccionamiento

--- src/main/java/com/proyecto/ecommerce/springecommerce/controllers/homeController.java
+++ src/main/java/com/proyecto/ecommerce/springecommerce/controllers/homeController.java
@@ -1,6 +1,5 @@
 package com.proyecto.ecommerce.springecommerce.controllers;
 
-
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -34,9 +33,9 @@ import com.proyecto.ecommerce.springecommerce.service.IUsuarioService;
 @Controller
 @RequestMapping("/")
 public class homeController {
-    
-    private final Logger log= LoggerFactory.getLogger(homeController.class);
-    
+
+    private final Logger log = LoggerFactory.getLogger(homeController.class);
+
     @Autowired
     private ProductoService productoService;
 
@@ -48,68 +47,79 @@ public class homeController {
 
     @Autowired
     private IDetalleOrdenService detalleOrdenService;
-    //Para almacenar los detalles de la orden 
+    // Para almacenar los detalles de la orden
     List<DetalleOrden> detalles = new ArrayList<DetalleOrden>();
 
-    //Metodo para obtener el idUsuario por la variable usuario
+    // Metodo para obtener el idUsuario por la variable usuario
     @ModelAttribute("idUsuarioOrden")
-    public Integer getIdUsuarioOrden(HttpSession session){
-        return Integer.parseInt(session.getAttribute("idusuario").toString());
-    } //para usarlo se usa como parametro : @ModelAttribute("idUsuarioOrden") Integer idUsuarioOrden
-    
-    //Datos de la orden
-    Orden orden =new Orden();
-    @GetMapping("") //apunta a la raiz
-    public String home(Model model, HttpSession session, @ModelAttribute("idUsuarioOrden") Integer idUsuarioOrden ){
-        log.info("Sesion del usuario (id) {}" , session.getAttribute("idusuario"));
-        model.addAttribute("productos", productoService.findAll()); //guardar todos los datos de productos en variable productos
-        //Variable sesion enviada a la vista
-        model.addAttribute("sesionUser", idUsuarioOrden);
+    public Integer getIdUsuarioOrden(HttpSession session) {
+        if (session != null && session.getAttribute("idusuario") != null) {
+            return Integer.parseInt(session.getAttribute("idusuario").toString());
+
+        }
+        return null;
+    } // para usarlo se usa como parametro : @ModelAttribute("idUsuarioOrden") Integer
+
+    // Datos de la orden
+    Orden orden = new Orden();
+
+    @GetMapping("") // apunta a la raiz
+    public String home(Model model, HttpSession session, @ModelAttribute("idUsuarioOrden") Integer idUsuarioOrden) {
+        if (idUsuarioOrden != null) {
+            model.addAttribute("productos", productoService.findAll()); // guardar todos los datos de productos en
+                                                                        // variable productos
+            // Variable sesion enviada a la vista
+            model.addAttribute("sesionUser", idUsuarioOrden);
+            log.info("Sesion del usuario (id) {}", session.getAttribute("idusuario"));
+            return "usuario/home";
+
+        }
+        model.addAttribute("productos", productoService.findAll()); // guardar todos los datos de productos en variable
+                                                                    // productos
         return "usuario/home";
     }
 
+    // Para ver detalle del producto
     @GetMapping("productohome/{id}")
-    public String productoHome(@PathVariable Integer id, Model model){
-        log.info("IdProducto enviado como parametro (vista cliente pag principal a detalle): {}", id );
+    public String productoHome(@PathVariable Integer id, Model model) {
+        log.info("IdProducto enviado como parametro (vista cliente pag principal a detalle): {}", id);
         Producto producto = new Producto();
         Optional<Producto> productoOptional = productoService.get(id);
         producto = productoOptional.get();
-        //Enviar lo del producto a la vista 
-        model.addAttribute("producto", producto); //"variable en vista", variable del metodo 
+        // Enviar lo del producto a la vista
+        model.addAttribute("producto", producto); // "variable en vista", variable del metodo
         return "usuario/productohome";
     }
 
     @GetMapping("/cart")
-    public String addcart(@RequestParam Integer id, @RequestParam Integer cantidad, Model model){
+    public String addcart(@RequestParam Integer id, @RequestParam Integer cantidad, Model model) {
         DetalleOrden detalleOrden = new DetalleOrden();
         Producto producto = new Producto();
-        double sumaTotal= 0;
-        
-        Optional<Producto> optionalProducto= productoService.get(id);
+        double sumaTotal = 0;
+
+        Optional<Producto> optionalProducto = productoService.get(id);
         log.info("Producto añadido {}" + optionalProducto.get());
         log.info("Cantidad de producto {}" + cantidad);
-        producto=  optionalProducto.get(); //del producto seleccionado asignarlo a la variable del producto
+        producto = optionalProducto.get(); // del producto seleccionado asignarlo a la variable del producto
 
         detalleOrden.setCantidad(cantidad);
         detalleOrden.setPrecio(producto.getPrecio());
         detalleOrden.setNombre(producto.getNombre());
-        detalleOrden.setTotal(producto.getPrecio()*cantidad);
-        detalleOrden.setProducto(producto);        
+        detalleOrden.setTotal(producto.getPrecio() * cantidad);
+        detalleOrden.setProducto(producto);
 
-
-        //Validacion para no registrar un mismo producto mas de 1 vez 
+        // Validacion para no registrar un mismo producto mas de 1 vez
         Integer idProducto = producto.getIdProducto();
-        boolean ingresado= detalles.stream().anyMatch(p -> p.getProducto().getIdProducto()== idProducto);
+        boolean ingresado = detalles.stream().anyMatch(p -> p.getProducto().getIdProducto() == idProducto);
 
-        if(!ingresado){
-            detalles.add(detalleOrden); //añadir cada detalle del orden a la lista 
+        if (!ingresado) {
+            detalles.add(detalleOrden); // añadir cada detalle del orden a la lista
         }
-        
-        
+
         sumaTotal = detalles.stream()
-    .mapToDouble(dt -> dt.getTotal()) // Mapea los valores de "total" a un stream de valores dobles
-    .sum(); // Calcula la suma de los valores dobles
-        
+                .mapToDouble(dt -> dt.getTotal()) // Mapea los valores de "total" a un stream de valores dobles
+                .sum(); // Calcula la suma de los valores dobles
+
         orden.setTotal(sumaTotal);
 
         model.addAttribute("cart", detalles);
@@ -117,23 +127,23 @@ public class homeController {
         return "usuario/carrito";
     }
 
-    //Quitar un producto del carro de compras
+    // Quitar un producto del carro de compras
     @GetMapping("/delete/cart/{id}")
-    public String deleteProducto(@PathVariable Integer id, Model model){
-       
-        //Lista nueva de productos 
-        List <DetalleOrden> ordenesNuevas= new ArrayList<DetalleOrden>();
+    public String deleteProducto(@PathVariable Integer id, Model model) {
 
-        for(DetalleOrden detalleOrden: detalles){
-            if(detalleOrden.getProducto().getIdProducto()!=id){
+        // Lista nueva de productos
+        List<DetalleOrden> ordenesNuevas = new ArrayList<DetalleOrden>();
+
+        for (DetalleOrden detalleOrden : detalles) {
+            if (detalleOrden.getProducto().getIdProducto() != id) {
                 ordenesNuevas.add(detalleOrden);
-                
+
             }
         }
-        //lista con los productos restantes:
-        detalles= ordenesNuevas;
-        
-        double sumaTotal= 0;
+        // lista con los productos restantes:
+        detalles = ordenesNuevas;
+
+        double sumaTotal = 0;
         sumaTotal = detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
         orden.setTotal(sumaTotal);
         model.addAttribute("cart", detalles);
@@ -142,19 +152,18 @@ public class homeController {
     }
 
     @GetMapping("/getCart")
-    public String getCart(Model model, @ModelAttribute("idUsuarioOrden") Integer idUsuarioOrden){
+    public String getCart(Model model, @ModelAttribute("idUsuarioOrden") Integer idUsuarioOrden) {
         model.addAttribute("cart", detalles);
         model.addAttribute("orden", orden);
-        //sesion
+        // sesion
         model.addAttribute("sesionUser", idUsuarioOrden);
         return "/usuario/carrito";
     }
 
     @GetMapping("/order")
-    public String order (Model model , @ModelAttribute("idUsuarioOrden") Integer idUsuarioOrden){
-        
-        Usuario usuario= usuarioService.findById(idUsuarioOrden).get();
+    public String order(Model model, @ModelAttribute("idUsuarioOrden") Integer idUsuarioOrden) {
 
+        Usuario usuario = usuarioService.findById(idUsuarioOrden).get();
 
         model.addAttribute("cart", detalles);
         model.addAttribute("orden", orden);
@@ -163,24 +172,24 @@ public class homeController {
     }
 
     @GetMapping("/saveOrder")
-    public String saveOrder(@ModelAttribute("idUsuarioOrden") Integer idUsuarioOrden){
+    public String saveOrder(@ModelAttribute("idUsuarioOrden") Integer idUsuarioOrden) {
         Date fechaCreacion = new Date();
         orden.setFechaCreacion(fechaCreacion);
         orden.setNumero(ordenService.generarNumeroOrden());
 
-        //usuario
-        Usuario usuario= usuarioService.findById(idUsuarioOrden).get();
-        
+        // usuario
+        Usuario usuario = usuarioService.findById(idUsuarioOrden).get();
+
         orden.setUsuario(usuario);
         ordenService.save(orden);
 
-        //Guardar Detalles
-        for(DetalleOrden dt: detalles){
+        // Guardar Detalles
+        for (DetalleOrden dt : detalles) {
             dt.setOrden(orden);
             detalleOrdenService.save(dt);
         }
-        
-        //limpiar lista y orden
+
+        // limpiar lista y orden
         orden = new Orden();
         detalles.clear();
 
@@ -188,11 +197,12 @@ public class homeController {
     }
 
     @PostMapping("/search")
-    public String searchProduct(@RequestParam String nombre, Model model){
-        log.info("Nombre del producto {}" , nombre);    
-        String nombreProducto = nombre.toLowerCase(); 
-        
-        List<Producto> productos = productoService.findAll().stream().filter(p -> p.getNombre().toLowerCase().contains(nombreProducto)).collect(Collectors.toList());
+    public String searchProduct(@RequestParam String nombre, Model model) {
+        log.info("Nombre del producto {}", nombre);
+        String nombreProducto = nombre.toLowerCase();
+
+        List<Producto> productos = productoService.findAll().stream()
+                .filter(p -> p.getNombre().toLowerCase().contains(nombreProducto)).collect(Collectors.toList());
         model.addAttribute("productos", productos);
         log.info("Nombre del producto seleccionado {} ", productos);
         return "usuario/home";
